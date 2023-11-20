5500b439532dd2c1ee15c2373b70d2b6bcc76418
glitchlover <samama.shahrin@gmail.com>
2023-11-20 10:50:27 +0600

    💎 style(🟣 App): changing the color of the highlights

--- lib/app/core/constants/color/highlights.dart
+++ lib/app/core/constants/color/highlights.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
 class ConstColorHighlight {
-  static const purple = Color(0xFFA27CFC);
-  static const blue = Color(0xFF656DF7);
-  static const lightBlue = Color(0xFF6DBEFF);
+  static const purple = Color.fromARGB(255, 157, 116, 255);
+  static const blue = Color.fromARGB(255, 109, 153, 255);
   static const green = Color(0xFF7CDA76);
   static const yellow = Color(0xFFDFD47A);
-  static const orange = Color(0xFFFEA677);
-  static const red = Color(0xFFF97C6D);
-  static const pink = Color(0xFFFF79B0);
+  static const orange = Color.fromARGB(255, 255, 157, 104);
+  static const red = Color.fromARGB(255, 250, 98, 98);
+  static const pink = Color.fromARGB(255, 253, 127, 179);
 }
